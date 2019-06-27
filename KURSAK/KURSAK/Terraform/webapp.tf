06b80fb7dd7a3c7dfcc4f6420c525320abf15891
alex <alex.antoniuk2@gmail.com>
2019-06-27 16:03:29 +0300

    add dev terraform

---
+++ KURSAK/KURSAK/Terraform/webapp.tf
@@ -0,0 +1,32 @@
+ terraform {
+  required_version = ">= 0.11" 
+ backend "azurerm" {
+  storage_account_name = "__kursovastorageaccount__"
+    container_name       = "kursova"
+    key                  = "kursova.tfstate"
+	access_key  ="__storagekey__"
+	}
+	}
+resource "azurerm_resource_group" "dev" {
+  name     = "kursowaapp"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "dev" {
+  name                = "__appserviceplan__"
+  location            = "${azurerm_resource_group.dev.location}"
+  resource_group_name = "${azurerm_resource_group.dev.name}"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "dev" {
+  name                = "__appservicename__"
+  location            = "${azurerm_resource_group.dev.location}"
+  resource_group_name = "${azurerm_resource_group.dev.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
+
+}
