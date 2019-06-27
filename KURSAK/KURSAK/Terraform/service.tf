98a61351cd6391b83336c493c7a09a35aaa210d9
alex <alex.antoniuk2@gmail.com>
2019-06-27 22:50:58 +0300

    commit del old terraform file

--- KURSAK/KURSAK/Terraform/service.tf
+++ KURSAK/KURSAK/Terraform/service.tf
@@ -1,14 +1,12 @@
 resource "azurerm_resource_group" "terra" {
-  name     = "${var.prefix}-resources"
-  location = var.location
+  name     = "example-resources"
+  location = "West Europe"
 }
 
 resource "azurerm_app_service_plan" "terra" {
-  name                = "${var.prefix}-asp"
-  location            = azurerm_resource_group.terra.location
-  resource_group_name = azurerm_resource_group.terra.name
-  kind                = "Linux"
-  reserved            = true
+  name                = "example-appserviceplan"
+  location            = "${azurerm_resource_group.terra.location}"
+  resource_group_name = "${azurerm_resource_group.terra.name}"
 
   sku {
     tier = "Standard"
@@ -16,20 +14,24 @@ resource "azurerm_app_service_plan" "terra" {
   }
 }
 
-resource "azurerm_app_service" "main" {
-  name                = "${var.prefix}-appservice"
-  location            = azurerm_resource_group.terra.location
-  resource_group_name = azurerm_resource_group.terra.name
-  app_service_plan_id = azurerm_app_service_plan.terra.id
+resource "azurerm_app_service" "terra" {
+  name                = "example-app-service"
+  location            = "${azurerm_resource_group.terra.location}"
+  resource_group_name = "${azurerm_resource_group.terra.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.terra.id}"
 
   site_config {
-    app_command_line = ""
-    linux_fx_version = "DOCKER|mcnac/lab3:latest"
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
   }
 
   app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io"
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
-
