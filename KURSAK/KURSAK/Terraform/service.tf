85ad5eb16e071fb0e7ec4fd65ebefe70953a405e
alex <alex.antoniuk2@gmail.com>
2019-06-27 22:17:20 +0300

    check file

--- KURSAK/KURSAK/Terraform/service.tf
+++ KURSAK/KURSAK/Terraform/service.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "terra" {
 
 resource "azurerm_app_service_plan" "terra" {
   name                = "${var.prefix}-asp"
-  location            = azurerm_resource_group.terra.location
-  resource_group_name = azurerm_resource_group.terra.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   kind                = "Linux"
   reserved            = true
 
@@ -18,9 +18,9 @@ resource "azurerm_app_service_plan" "terra" {
 
 resource "azurerm_app_service" "terra" {
   name                = "${var.prefix}-appservice"
-  location            = azurerm_resource_group.terra.location
-  resource_group_name = azurerm_resource_group.terra.name
-  app_service_plan_id = azurerm_app_service_plan.terra.id
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
 
   site_config {
     app_command_line = ""
